de89d79cdcd4d36f8bde53dbb0f9e1247eee2149
caindra <mmirgal868@g.educaand.es>
2024-10-28 21:52:58 +0100

    first 'hello world' app created

---
+++ 01-ts-introduction/src/main.ts
@@ -0,0 +1,8 @@
+import './style.css'
+import { setupCounter } from './counter.ts'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `Hola mundo!`
+
+console.log('Hola mundo!')
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
