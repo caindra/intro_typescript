c30fbf9dcf144b9df9d69a9c37770ec859960f63
caindra <mmirgal868@g.educaand.es>
2024-11-03 17:53:28 +0100

    objects as function's parameters

--- 01-ts-introduction/src/topics/03-functions.ts
+++ 01-ts-introduction/src/topics/03-functions.ts
@@ -13,10 +13,36 @@ addNumbers(1, 2);
 
 const result: number = addNumbers(1, 2);
 const result2: string = addNumbers(1, 2).toString();
-const multiplyResult = multiply(5);
-
+const multiplyResult: number = multiply(5);
+/*
 console.log({ result: result });
-console.log({ result });
+console.log({ result, result2, multiplyResult });
 console.log({ addNumbersArrow });
+*/
+
+interface Character {
+    name: string;
+    hp: number;
+    showHp: () => void;
+}
+
+const healCharacter = (character: Character, amount: number) => {
+    character.hp += amount;
+}
+
+const strider: Character = {
+    name: 'Strider',
+    hp: 50,
+    showHp() {
+        console.log(`Puntos de vida: ${this.hp}`);
+    }
+}
+
+strider.showHp();
+healCharacter(strider, 10);
+strider.showHp();
+healCharacter(strider, 50);
+strider.showHp();
+
 
 export {};
