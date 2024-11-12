82d40649d50ad7dbea6d5363ad2d9f59a7a1375b
caindra <mmirgal868@g.educaand.es>
2024-11-12 19:23:04 +0100

    extend from a class + prioritase heritage

--- 01-ts-introduction/src/topics/08-classes.ts
+++ 01-ts-introduction/src/topics/08-classes.ts
@@ -3,14 +3,49 @@ export class Person {
     //private address: string;
 
     constructor(
-        public name: string, 
+        public firstName: string, 
+        public lastName: string, 
         private address: string = 'No address'
     ) {
-        this.name = name;
-        this.address = address;
+        //this.firstName = firstName;
+        //this.lastName = lastName;
+        //this.address = address;
     }
 }
 
-const ironman = new Person('Caindra', 'Neverland');
+/*
+export class Hero extends Person {
+    constructor (
+        public alterEgo: string,
+        public age: number,
+        public realName: string
+    ){
+        super(realName, 'New York');
+        this.alterEgo = alterEgo;
+        this.age = age;
+        this.realName = realName;
+    }
+}
+*/
+
+export class Hero {
+
+    //public person: Person;
+
+    constructor (
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+        public person: Person
+    ){
+        //this.person = new Person(realName);
+        this.alterEgo = alterEgo;
+        this.age = age;
+    }
+}
+
+const person = new Person('Caindra', 'Watts', 'Neverland');
+//console.log(person);
+const ironman = new Hero('IronMan', 45, 'Tony', person);
 console.log(ironman);
 //console.log(ironman.address);
